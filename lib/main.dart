1b1fc0e0df02b023ee45544d406025c3738cf9b3
Fernanda Souza <fernanda.fernandes10@fatec.sp.gov.br>
2021-05-20 21:00:51 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,18 @@
+/*Nome: Fernanda Cristina de Souza Fernandes
+//RA: 2920482011032
+88. Utilizando o aplicativo da aula 14 – Gif Animados,
+fazer a seguinte alteração:
+
+I. Proponha e faça alterações no Aplicativo (no mínimo 3
+alterações).
+89.Descreva as alterações que foram feitas no aplicativo. (item I)
+  1. Alteração da cor do appBar para pink.shade200
+  2. Alteração da cor do background das telas para lightBlue
+  3. Alteração da cor do texto de carregamento para purple
+  4. Alteração da quantidade de gifs por linha para três
+  5. Alteração da cor do texto de "Pesquisa Aqui" para purple.shade900
+
+ */
 import 'package:flutter/material.dart';
 import 'package:flutter_gifs/ui/HomePage.dart';
 import 'ui/HomePage.dart';
